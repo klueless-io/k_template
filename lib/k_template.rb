1fc45f9c98c9445c65d7f307e67aa9912b87ff82
David Cruwys <david@ideasmen.com.au>
2024-10-11 08:18:12 +0700

    first commit

---
+++ lib/k_template.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "k_template/version"
+
+module KTemplate
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/k_template/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module KTemplate
+  VERSION = "0.1.0"
+end
