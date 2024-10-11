16b8567e2f420a4e126851c880a08533a29ae7e1
David Cruwys <david@ideasmen.com.au>
2024-10-11 08:25:06 +0700

    fix: test ci

--- lib/ktemplate/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module KTemplate
-  VERSION = '0.0.1'
-end
--- spec/k_template_spec.rb
+++ spec/k_template_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe KTemplate do
   it 'has a version number' do
     expect(KTemplate::VERSION).not_to be_nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
